b847bb9a7b77deb4ac7b3750322beb371210a229
Nadya Huanca <lhuanca500@gmail.com>
2025-08-27 07:13:59 -0400

    feat: agregar instancia global de PrismaClient

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForprisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForprisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForprisma.prisma = prisma;
